75d2b3e59c6bb27d3aa25eb4661aedb0a9a8167b
mihalceamihai <mihalcea.mihaialexandru@gmail.com>
2021-06-28 01:15:25 +0300

    modify table structure

--- src/main/java/com/example/postgres/springbootpostgresdocker/controller/MetricsController.java
+++ src/main/java/com/example/postgres/springbootpostgresdocker/controller/MetricsController.java
@@ -4,23 +4,29 @@ import com.example.postgres.springbootpostgresdocker.MetricsRepository;
 import com.example.postgres.springbootpostgresdocker.model.Metrics;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+//@RestController
+@Controller
+@CrossOrigin(allowedHeaders = "*", origins = "*")
 public class MetricsController {
 
     @Autowired
     private MetricsRepository metricsRepository;
 
     @PostMapping("/metrics")
+    @ResponseBody
     public Metrics addMetrics(@RequestBody Metrics metrics){
 
        return metricsRepository.save(metrics);
 
     }
     @GetMapping("/metrics")
+    @ResponseBody
     public ResponseEntity<List<Metrics>> findAll(){
 
        return ResponseEntity.ok(metricsRepository.findAll());
@@ -28,6 +34,7 @@ public class MetricsController {
     }
 
     @GetMapping("/metrics/{id}")
+    @ResponseBody
     public ResponseEntity<Metrics> findMetricId(@PathVariable(value = "id") Integer id){
 
         Metrics metrics=metricsRepository.findById(id).orElseThrow( ()-> new ResourceNotFoundException("Metric not found"
@@ -36,6 +43,7 @@ public class MetricsController {
     }
 
     @DeleteMapping("/metrics/{id}")
+    @ResponseBody
     public ResponseEntity<Void> deleteMetric(@PathVariable(value = "id") Integer id){
         Metrics metrics=metricsRepository.findById(id).orElseThrow( ()-> new ResourceNotFoundException("Metric not found"
                 + id));
@@ -43,4 +51,9 @@ public class MetricsController {
         return ResponseEntity.ok().build();
     }
 
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String viewHomePage(Model model){
+        model.addAttribute("listMetrics",metricsRepository.findAll());
+        return "index";
+    }
 }
